ee30ba7daa616b78f0d0419dd89a777f63c5e5dd
erikvw <ew2789@gmail.com>
2016-08-23 22:25:19 +0200

    update configparser import

--- edc_sync_files/apps.py
+++ edc_sync_files/apps.py
@@ -1,33 +1,26 @@
 import sys
 
 from django.apps import AppConfig as DjangoAppConfig
-from django.conf import settings
 from django.core.management.color import color_style
 
-from edc_base.config_parser_mixin import ConfigParserMixin
+from django_appconfig_ini.mixin import ConfigIniMixin
 
 style = color_style()
 
 
-class AppConfig(ConfigParserMixin, DjangoAppConfig):
+class AppConfig(ConfigIniMixin, DjangoAppConfig):
     name = 'edc_sync_files'
     verbose_name = 'File Synchronization'
-    role = 'server'
     device_ip = 'localhost'
     source_folder = '~/edc_sync_files'
     destination_folder = None
     media_folders = []
-    config_filename = 'edc_sync_files.ini'
-
+    config_ini_attrs = {'edc_sync_files': ['source_folder', 'destination_folder', 'media_folders', 'device_ip']}
     cors_origin_whitelist = None  # a tuple of host:port, host:port, ...
     cors_origin_allow_all = True
 
     def ready(self):
         sys.stdout.write('Loading {} ...\n'.format(self.verbose_name))
-        if not self.role:
-            sys.stdout.write(style.NOTICE(
-                ' Warning: Project uses \'edc_sync_files\' but has not defined a role for this '
-                'app instance. See AppConfig.\n'))
-        self.overwrite_config_attrs_on_class(self.name)
+        self.overwrite_config_ini_attrs_on_class(self.name)
         sys.stdout.write(' * role is {}.\n'.format(self.role.upper()))
         sys.stdout.write(' Done loading {}.\n'.format(self.verbose_name))
