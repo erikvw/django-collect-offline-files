1a3557035f2b1f6ad4f3e390c92315fe34e9f331
Tshepiso Setsiba <tsetsiba@bhp.org.bw>
2017-03-28 12:02:07 +0200

    tests

--- edc_sync_files/tests/test_file_event_handler.py
+++ edc_sync_files/tests/test_file_event_handler.py
@@ -20,23 +20,21 @@ class testFileEventHandler(TestCase):
         # Create transactions
         tx = TestModel.objects.using('client').create(f1=self.fake.name())
         tx1 = TestModel.objects.using('client').create(f1=self.fake.name())
-        
+
         tx.delete()
         tx1.delete()
-        
+
         self.assertEqual(TestModel.objects.using('client').filter(f1=tx.f1).count(), 0)
         self.assertEqual(TestModel.objects.using('client').filter(f1=tx1.f1).count(), 0)
 
         # Dump transaction
         path = '/Users/tsetsiba/source/edc-sync-files/edc_sync_files/media/transactions/incoming'
         TransactionDumps(path, using='client', hostname="010")
-        
         self.assertEqual(TestModel.objects.using('default').filter(f1=tx.f1).count(), 1)
         self.assertEqual(TestModel.objects.using('default').filter(f1=tx1.f1).count(), 1)
-        
-        self.assertEqual(UploadTransactionFile.objects.all().count(), 1) # Uploaded by watchdog
-        
-        
+
+        self.assertEqual(UploadTransactionFile.objects.all().count(), 1)  # Uploaded by watchdog
+
     def test_export_upload_multiple_files(self):
         # Create transactions
         tx = TestModel.objects.using('client').create(f1=self.fake.name())
@@ -47,12 +45,11 @@ class testFileEventHandler(TestCase):
         # Dump transaction
         path = '/Users/tsetsiba/source/edc-sync-files/edc_sync_files/media/transactions/incoming'
         TransactionDumps(path, using='client', hostname="010")
-        
         self.assertEqual(TestModel.objects.using('default').filter(f1=tx.f1).count(), 1)
         self.assertEqual(TestModel.objects.using('default').filter(f1=tx1.f1).count(), 1)
-        
+
         self.assertEqual(UploadTransactionFile.objects.all().count(), 1)
-        
+
         # Create transactions
         tx = TestModel.objects.using('client').create(f1=self.fake.name())
         tx1 = TestModel.objects.using('client').create(f1=self.fake.name())
@@ -62,7 +59,3 @@ class testFileEventHandler(TestCase):
 
         TransactionDumps(path, using='client', hostname="010")
         self.assertEqual(UploadTransactionFile.objects.all().count(), 1)
-        
-        
-        
-        
