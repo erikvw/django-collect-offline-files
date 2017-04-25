7fbf8c18b50ee66e9e98a13b28b6075168fe7f62
Tshepiso Setsiba <tsetsiba@bhp.org.bw>
2017-04-25 22:15:55 +0200

    fix bug on observer

--- edc_sync_files/management/commands/start_observer.py
+++ edc_sync_files/management/commands/start_observer.py
@@ -1,5 +1,6 @@
 import sys
 
+from django.apps import apps as django_apps
 from django.core.management.base import BaseCommand
 
 from ...event_handlers import TransactionFileEventHandler
@@ -10,10 +11,13 @@ class Command(BaseCommand):
     help = 'Start watchdog observer'
 
     def handle(self, *args, **options):
-
+        destination_folder = django_apps.get_app_config(
+            'edc_sync_files').destination_folder
         observer = Observer()
-        observer.start(event_handler_class=TransactionFileEventHandler)
+        observer.start(
+            event_handler_class=TransactionFileEventHandler,
+            path=destination_folder)
 
         sys.stdout.write('Upload folder: {}\n'.format(
-            observer.event_handler.destination_folder))
+            destination_folder))
         sys.stdout.write('\npress CTRL-C to stop.\n\n')
